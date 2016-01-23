24d293546cafc659753bc409385a2d52288557f9
Nikolay Mateev <bgdunkers@gmail.com>
2016-01-23 12:53:34 +0200

    Added exceptions

--- Panda Social Network/PandaClassLibrary/PandaExceptions.cs
+++ Panda Social Network/PandaClassLibrary/PandaExceptions.cs
@@ -13,4 +13,26 @@ namespace PandaClassLibrary
 
         protected InvalidPandaEmailException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
+
+    public class PandaAlreadyThereException : ApplicationException
+    {
+        public PandaAlreadyThereException() : this("Panda already exists in the network!") { }
+
+        public PandaAlreadyThereException(string message) : base(message) { }
+
+        public PandaAlreadyThereException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected PandaAlreadyThereException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+
+    public class PandasAlreadyFriendsException : ApplicationException
+    {
+        public PandasAlreadyFriendsException() : this("These panda are already friends.") { }
+
+        public PandasAlreadyFriendsException(string message) : base(message) { }
+
+        public PandasAlreadyFriendsException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected PandasAlreadyFriendsException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
 }
